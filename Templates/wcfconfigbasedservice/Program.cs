304399cde4cd69303bf57dd1a87d0e4e89a64f25
Kanikaul <kanikaul@amazon.com>
2021-09-22 18:55:32 -0700

    Add WCF Recommendations and Templates

---
+++ Templates/wcfconfigbasedservice/Program.cs
@@ -0,0 +1,25 @@
+/*
+using CoreWCF.Configuration;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace #NAMESPACEPLACEHOLDER#
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+      //All Ports set are default.
+			IWebHost host = CreateWebHostBuilder(args).Build();
+      host.Run();
+		}
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+      WebHost.CreateDefaultBuilder(args)
+				 .UseKestrel(options => { })
+				 .UseStartup<Startup>();
+	}
+}
+*/
