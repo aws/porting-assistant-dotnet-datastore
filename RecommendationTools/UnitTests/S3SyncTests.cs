f1919c7c1a09c9c371cc2264ae950c56ef314491
Beau Gosse <bgosse@amazon.com>
2022-02-21 22:12:14 -0600

    always use master branch

--- RecommendationTools/UnitTests/S3SyncTests.cs
+++ RecommendationTools/UnitTests/S3SyncTests.cs
@@ -1,27 +1,46 @@
 using NUnit.Framework;
 using System;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PostSyncTests
 {
     /// <summary>
-    /// These tests are made to be run from the main branch after a merge and sync to the public S3 bucket. This is needed
+    /// These tests are made to be run automatically after a merge and sync to the public S3 bucket. This is needed
     /// in order to validate that the public S3 bucket is up to date with the repository and that the sync is working.
     /// If running these tests in a feature branch with changes to the recommendation files, the tests should fail.
     /// </summary>
     public class S3SyncTests
     {
         private const string S3UrlPrefix = "https://s3.us-west-2.amazonaws.com/aws.portingassistant.dotnet.datastore/recommendationsync/recommendation";
+        private const string TempExtractDir = "masterBranchCode";
+
+        [TearDown]
+        [SetUp]
+        public static void SetupAndTearDown()
+        {
+            if (Directory.Exists(TempExtractDir))
+            {
+                Directory.Delete(TempExtractDir, true);
+            }
+        }
 
         [Test]
-        public void ValidateS3RecommendationContentMatchesRepository()
+        public async Task ValidateS3RecommendationContentMatchesRepository()
         {
-            var pathToRepoRecs = FindDirectoryUpwards("recommendation");
+            using var httpClient = new HttpClient();
+            // Use always master branch since that is the only branch that should be in sync with S3
+            var zipBytes = await httpClient.GetByteArrayAsync("https://github.com/aws/porting-assistant-dotnet-datastore/archive/refs/heads/master.zip");
+            await File.WriteAllBytesAsync("github.zip", zipBytes);
+            Directory.CreateDirectory(TempExtractDir);
+            ZipFile.ExtractToDirectory("github.zip", "masterBranchCode");
+            var pathToRepoRecs = $@"{TempExtractDir}\porting-assistant-dotnet-datastore-master\recommendation";
+
             var allRepoRecs = Directory.EnumerateFiles(pathToRepoRecs).ToList();
 
-            using var httpClient = new HttpClient();
             Assert.Multiple(async () =>
             {
                 foreach(var file in allRepoRecs)
@@ -34,24 +53,6 @@ namespace PostSyncTests
                     Assert.AreEqual(localContent, s3Content, $"Content didn't match for file {file}");
                 }
             });
-        }
-
-        private static string FindDirectoryUpwards(string searchPattern)
-        {
-            var fullPath = string.Empty;
-            var currentDir = Directory.GetCurrentDirectory();
-            while (string.IsNullOrEmpty(fullPath) && currentDir != null)
-            {
-                var matchingDir = Directory.GetDirectories(currentDir, "recommendation").FirstOrDefault();
-
-                if (matchingDir != null)
-                {
-                    return matchingDir;
-                }
-                currentDir = Directory.GetParent(currentDir)?.FullName;
-            }
-
-            throw new InvalidOperationException($"Couldn't find directory with name {searchPattern} when looking upward from current directory");
         }
     }
 }
